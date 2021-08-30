3a2731e5b607082bf021324cc0645216ba1e3f4c
GeorgiiProjects <89801261+GeorgiiProjects@users.noreply.github.com>
2021-08-30 21:42:43 +0300

    Prototype2_2D
    Prototype2_2D

---
+++ Scripts/AdventureGame.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // создаем класс (переменную) Text, чтобы в него можно было поместить текст в инспекторе.
+    [SerializeField] Text textComponent;
+    // создаем класс (переменную) State, чтобы в него можно было поместить файл StartingState с расширением asset в инспекторе.
+    [SerializeField] State startingState;
+    // создаем класс State для того чтобы получить доступ к скрипту State.
+    State state;
+
+    void Start()
+    {
+        // передаем текст из StartingState в переменную (поле) state.
+        state = startingState;
+        // получаем доступ в textComponent к тексту Story Text из Canvas, вызываем метод GetStateStory() из скрипта State.
+        // и запускаем отображение текста из файла StartingState при старте игры.
+        textComponent.text = state.GetStateStory();
+    }
+
+    void Update()
+    {
+        // вызываем каждый фрейм метод ManageState()
+        ManageState();
+    }
+
+    // Создаем метод ManageState(), для того чтобы можно было выбрать комнату из массива для перехода между ними.
+    private void ManageState()
+    {
+        // используем var, так как nextStates является массивом (можно использовать State[]), вызываем GetNextStates() из скрипта State.
+        // передаем значение в переменную массива nextStates.
+        var nextStates = state.GetNextStates();
+
+        // начинаем отсчет комнат State с элемента индекса ноль, пока индекс < количества вариантов выбора комнат, индекс сможет увеличиваться на 1.
+        // для первой комнаты индекс будет только один так как только один вариант развития, 
+        // для второй уже два и.т.д в зависимости от количества выбора вариантов в комнате.
+        for(int index = 0; index<nextStates.Length; index++)
+        {
+            // Сможем переходить между комнатами при нажатии кнопок 1 и 2
+            // Если мы нажимаем кнопку 1 + индекс номера комнаты (0,1)
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                // переходим с комнаты StartingState в комнату RoomOne или RoomTwo.
+                state = nextStates[index];
+            }
+        }  
+
+        // Запускаем смену текста в комнатах на в игре через кнопки 1,2, и.т.д. клавиатуры.
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Scripts/State.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+// позволяет в Unity создать (Create-State) новый ScriptableObject с именем по умолчанию State с расширением asset.
+[CreateAssetMenu(menuName = "State")]
+// создаем класс ScriptableObject, который будет содержать/хранить в себе текст и не будет связан ни с каким GameObject в инспекторе.
+public class State : ScriptableObject
+{
+    // [TextArea(14, 10)] создаем размер текстового поля 14 строк (текст который будет виден сразу), 10 через сколько строк можно будет скролить текст, 
+    // который виден в инспекторе в файлах .asset.
+    // создаем класс string который будет виден в инспекторе, в файлах .asset, позволяет писать текст прямо в инспекторе.
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    // создаем массив типа State, который будет виден в инспекторе, в файлах .asset, для того чтобы поместить в него файлы .asset.
+    [SerializeField] State[] nextStates;
+
+    // Создаем публичный метод GetStateStory(), для того чтобы отобразить текст на экране, при его вызове в скрипте AdventureGame.
+    public string GetStateStory()
+    {
+        // отображаем текст на экране.
+        return storyText;
+    }
+
+    // Создаем метод GetNextStates(), для того чтобы вызвать его в скрипте AdventureGame и использовать выбор вариантов прохождения из массива.
+    public State[] GetNextStates()
+    {
+        // переходим в различные комнаты.
+        return nextStates;
+    }
+}
